446929878c13812a831fe23101ba4281db37aa84
Spoorloos <mick.negenman@icloud.com>
2025-12-11 13:07:09 +0100

    Language picker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import LocaleSwitcher from "@/components/LocaleSwitcher";
+import { localeCodes } from "@/lib/localization";
+import { getUser } from "@/lib/auth";
 
 export default async function Header() {
+    const user = await getUser();
+
     return (
         <header className="bg-black text-white flex justify-between items-center p-4">
             <Link href="/">
                 <p className="text-xl font-bold">Adseum</p>
             </Link>
-            <nav>
-            </nav>
+            <div className="flex items-stretch gap-2">
+                <nav>
+                    {user !== null && (
+                        <Link className="block p-2 bg-yellow-500" href="/admin">Admin</Link>
+                    )}
+                </nav>
+                <LocaleSwitcher localeCodes={localeCodes as unknown as string[]} />
+            </div>
         </header>
     )
 }
---
+++ src/components/LocaleSwitcher.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useTranslations } from "./TranslationProvider";
+import Link from "next/link";
+
+const stripLocaleRegex = /^\/[^\/]+/;
+
+export default function LocaleSwitcher({ localeCodes }: { localeCodes: string[] }) {
+    const { localeCode } = useTranslations();
+    const [open, setOpen] = useState(false);
+    const pathName = usePathname().replace(stripLocaleRegex, "");
+
+    return (
+        <div className="relative">
+            <button
+                className="bg-red-500 p-2 flex gap-1 items-center cursor-pointer uppercase"
+                onClick={() => setOpen(x => !x)}
+            >
+                {localeCode}
+                <ChevronDown/>
+            </button>
+            {open && (
+                <ul className="absolute top-full left-0 w-full max-h-32 overflow-y-auto">
+                    {localeCodes.map((code) => (
+                        <li key={code}>
+                            <a className="block p-1 uppercase bg-blue-500 hover:bg-blue-600" href={`/${code}${pathName}`}>
+                                {code}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
