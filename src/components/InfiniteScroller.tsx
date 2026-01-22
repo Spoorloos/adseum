e08573fb277f679e9b347b86a59e7bfa769cf4e3
Spoorloos <mick.negenman@icloud.com>
2026-01-22 13:25:10 +0100

    Minor fix to infinite scroller

--- src/components/InfiniteScroller.tsx
+++ src/components/InfiniteScroller.tsx
@@ -12,12 +12,12 @@ export default function InfiniteScroller({ children, speed, direction }: Infinit
 
     return (
         <div className="overflow-hidden max-w-full">
-            <ul className="relative inline-flex gap-4 whitespace-nowrap w-max animate-[infinite-scroll_linear_infinite]" style={{
+            <ul className="relative inline-flex gap-4 whitespace-nowrap pr-4 w-max animate-[infinite-scroll_linear_infinite]" style={{
                 animationDuration: speed ?? "7s",
                 animationDirection: direction ?? "normal",
             }}>
                 {items}
-                <ul className="absolute left-[calc(100%+var(--spacing)*4)] inline-flex gap-4 whitespace-nowrap w-max">{items}</ul>
+                <ul className="absolute left-full inline-flex gap-4 whitespace-nowrap w-max">{items}</ul>
             </ul>
         </div>
     );
